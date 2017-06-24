bfdec92eee757a50700a0c5f91470e2eaee8c257
CodeGood <codegood.io@gmail.com>
2017-06-24 09:24:03 +0000

    Initial commit: plugin header.

---
+++ wp-simple-event-listing.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: WP Simple Event Listing
+Plugin URI:  https://github.com/qriouslad/wp-simple-event-listing
+Description: Simple event listing plugin for WordPress
+Version:     1.0
+Author:      Bowo
+Author URI:  https://bowo.io
+Text Domain: wpsel
+Domain Path: /languages
+License:     GPL2
+ 
+WP Simple Event Listing is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+WP Simple Event Listing is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with {Plugin Name}. If not, see {License URI}.
+*/
